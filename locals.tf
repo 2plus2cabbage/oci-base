39769414c44510801097d6048cc7021abc652559
2+2=cabbage <2plus2cabbage@gmail.com>
2025-07-07 19:09:12 -0400

    Update availability domain

--- locals.tf
+++ locals.tf
@@ -4,5 +4,5 @@ locals {
   windows_name_prefix          = "vm-${var.environment_name}-${var.location}-windows-"             # Prefix for Windows VM name
   security_list_name_prefix    = "slist-${var.environment_name}-${var.location}-"                  # Prefix for security list name
   internet_gateway_name_prefix = "igw-${var.environment_name}-${var.location}-"                    # Prefix for internet gateway name
-  route_table_name_prefix      = "rt-${var.environment_name}-${var.location}-"                     # Prifix for route table name
+  route_table_name_prefix      = "rt-${var.environment_name}-${var.location}-"                     # Prefix for route table name
 }
